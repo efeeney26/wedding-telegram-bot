0445b06f28a335873a1d5518e657cfddfdad94c0
Kochkin Danila <>
2022-05-18 00:53:37 +0300

    actions

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { config } from 'dotenv';
-import { Context, Telegraf } from 'telegraf';
+import { Context, Markup, Telegraf } from 'telegraf';
 import { Update } from 'typegram';
+
+import { FACTS, ACTS } from './constants';
 
 config();
 
@@ -8,8 +10,35 @@ const token: string = process.env.BOT_TOKEN as string;
 
 const bot: Telegraf<Context<Update>> = new Telegraf(token);
 
-bot.start((ctx) => {
-  ctx.reply(`Привет ${ctx.from.first_name}!`);
+const inlineKeyboard = Markup.inlineKeyboard([
+  Markup.button.callback('Получить забавный факт о молодоженах', 'fact'),
+  Markup.button.callback('Получить задание', 'act'),
+  Markup.button.callback('Отправить пожелание молодоженам', 'wish'),
+], {
+  columns: 1,
 });
+
+bot.start((ctx) => {
+  ctx.reply(
+    `Привет, ${ctx.from.first_name}! Смотри, что я умею:`,
+    inlineKeyboard
+  );
+});
+
+bot.action('fact', async (ctx, next) => {
+  await ctx.reply(
+    `Ты удивишься, но... 🌚🌚 ${FACTS[Math.floor(Math.random() * FACTS.length)]} 🌚🌚`,
+    inlineKeyboard,
+  );
+  next();
+})
+
+bot.action('act', async (ctx, next) => {
+  await ctx.reply(
+    `Докажи, насколько ты крут. 😎😎 ${ACTS[Math.floor(Math.random() * FACTS.length)]} 😎😎`,
+    inlineKeyboard
+  );
+  next();
+})
 
 bot.launch();
