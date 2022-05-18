bad0ef65007e5235bf9239b57adf673a75ad9cc8
Kochkin Danila <>
2022-05-18 15:27:06 +0300

    feat: proxy message

--- src/index.ts
+++ src/index.ts
@@ -41,4 +41,20 @@ bot.action('act', async (ctx, next) => {
   next();
 })
 
+let conditionToStopEaringMessages: boolean;
+
+bot.action('wish', async (ctx) => {
+  conditionToStopEaringMessages = false;
+  await ctx.reply('Напиши пожелание молодоженам');
+  bot.on('text', async (ctx) => {
+    if (ctx.message.text && !conditionToStopEaringMessages) {
+        const message = await ctx.copyMessage('-720490123', ctx.message);
+        if (message) {
+          await ctx.reply('Сообщение отправлено молодоженам! Спасибо!', inlineKeyboard);
+        }
+        conditionToStopEaringMessages = true;
+      }
+  });
+})
+
 bot.launch();
